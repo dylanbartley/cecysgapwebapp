15335d02444a9cb74171d7a0ee9a69e0aa33e853
Dylan Bartley <d.bartley44@gmail.com>
2019-04-10 15:10:59 +0000

    moved: main.js logic into jquery ready function

--- src/main.js
+++ src/main.js
@@ -4,15 +4,17 @@ require('./css/theme.css');
 
 const $ = require('jquery/dist/jquery.slim.min.js');
 
-// main application object
-window.cgapp = require('./js/app');
-
-// show modal which are hidden by default, they are translated out of view
-// otherwise they will animate down on load
-$('.app-modal').css('display', 'block'); 
-
-// update copyright years
-const startYr = 2019;
-const thisYr = (new Date()).getFullYear();
-
-$('#copyrightYr').text(startYr === thisYr ? startYr : startYr + '-' + thisYr);
+$(function () {
+  // main application object
+  window.cgapp = require('./js/app');
+  
+  // show modal which are hidden by default, they are translated out of view
+  // otherwise they will animate down on load
+  $('.app-modal').css('display', 'block'); 
+  
+  // update copyright years
+  const startYr = 2019;
+  const thisYr = (new Date()).getFullYear();
+  
+  $('#copyrightYr').text(startYr === thisYr ? startYr : startYr + '-' + thisYr);
+});
