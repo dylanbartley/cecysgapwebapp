5ff5df8ed7e5e99d0e1084359887190f98f58620
Dylan Bartley <d.bartley44@gmail.com>
2019-04-14 10:41:44 +0000

    added: clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
@@ -48,6 +50,7 @@ module.exports = env => {
       ]
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: env ? '[name].[hash].css' : '[name].css'
       }),
